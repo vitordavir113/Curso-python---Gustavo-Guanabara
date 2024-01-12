c52b6d6948529cd670ce7f57e1b4828142cd8f63
Vitor <vitordavir113@gmail.com>
2024-01-12 20:55:02 -0300

    Add files via upload

---
+++ 002.py
@@ -0,0 +1,4 @@
+nome = input('Digite seu nome')
+print('seja bem vindo,', nome)
+
+
---
+++ 003.py
@@ -0,0 +1,5 @@
+n1 = int(input('Digite um numero'))
+n2 = int(input('Digite outro numero'))
+s =  n1 + n2
+#print('A soma entre', n1, 'e', n2, 'é igual a', s)
+print('a soma entre {} e {} e igual a {}'.format(n1, n2, s))
